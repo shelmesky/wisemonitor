9214ee9a5730fc10f1e17f590351a019ccaea47f
roy <roy.lieu@gmail.com>
2013-09-16 12:59:50 +0800

    在get_host_info中增加详细信息的标志，调用者可以选择是否获取存储和网络设备的信息

--- virtualization/xenserver.py
+++ virtualization/xenserver.py
@@ -5,7 +5,7 @@ from pprint import pprint
 import XenAPI
 
 
-def get_host_info(session, host_record):
+def get_host_info(session, host_record, verbose=True):
     temp_record = {}
     record = session.xenapi.host.get_record(host_record)
     temp_record['uuid'] = record['uuid']
@@ -15,51 +15,52 @@ def get_host_info(session, host_record):
     temp_record['cpu_modelname'] = record['cpu_info']['modelname']
     temp_record['version'] = record['software_version']['product_version']
     
-    # get all PBDs
-    temp_record['PBDs'] = []
-    pbds = record['PBDs']
-    j = 1
-    for pbd in pbds:
-        temp_pbd = {}
-        pbd_record = session.xenapi.PBD.get_record(pbd)
-        sr = pbd_record['SR']
-        sr_record = session.xenapi.SR.get_record(sr)
-        temp_pbd['pbd_attached'] = pbd_record['currently_attached']
-        temp_pbd['pbd_device_config'] = str(pbd_record['device_config'])
-        temp_pbd['sr_name'] = sr_record['name_label']
-        temp_pbd['sr_size'] = str(int(sr_record['physical_size']) / (1024**3))
-        temp_pbd['sr_allocation'] = str(int(sr_record['virtual_allocation']) / (1024**3))
-        temp_pbd['sr_utilisation'] = str(int(sr_record['physical_utilisation']) / (1024**3))
-        temp_pbd['sr_type'] = sr_record['type']
-        temp_pbd['id'] = j
-        j += 1
-        temp_record['PBDs'].append(temp_pbd)
-    
-    # get all PIFs
-    temp_record['PIFs'] = []
-    pifs = record['PIFs']
-    k = 1
-    for IF in pifs:
-        temp_pif = {}
-        IF_record = session.xenapi.PIF.get_record(IF)
-        temp_pif['ip_config_mode'] = IF_record['ip_configuration_mode']
-        temp_pif['is_physical'] = IF_record['physical']
-        temp_pif['device'] = IF_record['device']
-        temp_pif['is_management'] = IF_record['management']
-        temp_pif['IP'] = IF_record['IP']
-        temp_pif['netmask'] = IF_record['netmask']
-        temp_pif['MAC'] = IF_record['MAC']
-        temp_pif['VLAN'] = IF_record['VLAN']
-        temp_pif['id'] = k
-        k += 1
-        temp_record['PIFs'].append(temp_pif)
-    
     # get memory parameters in metrics field
     metrics = record['metrics']
     metrics_record = session.xenapi.host_metrics.get_record(metrics)
     temp_record['memory_total'] = int(metrics_record['memory_total']) / (1024**2)
     temp_record['memory_free'] = int(metrics_record['memory_free']) / (1024**2)
     
+    if verbose == True:
+        # get all PBDs
+        temp_record['PBDs'] = []
+        pbds = record['PBDs']
+        j = 1
+        for pbd in pbds:
+            temp_pbd = {}
+            pbd_record = session.xenapi.PBD.get_record(pbd)
+            sr = pbd_record['SR']
+            sr_record = session.xenapi.SR.get_record(sr)
+            temp_pbd['pbd_attached'] = pbd_record['currently_attached']
+            temp_pbd['pbd_device_config'] = str(pbd_record['device_config'])
+            temp_pbd['sr_name'] = sr_record['name_label']
+            temp_pbd['sr_size'] = str(int(sr_record['physical_size']) / (1024**3))
+            temp_pbd['sr_allocation'] = str(int(sr_record['virtual_allocation']) / (1024**3))
+            temp_pbd['sr_utilisation'] = str(int(sr_record['physical_utilisation']) / (1024**3))
+            temp_pbd['sr_type'] = sr_record['type']
+            temp_pbd['id'] = j
+            j += 1
+            temp_record['PBDs'].append(temp_pbd)
+        
+        # get all PIFs
+        temp_record['PIFs'] = []
+        pifs = record['PIFs']
+        k = 1
+        for IF in pifs:
+            temp_pif = {}
+            IF_record = session.xenapi.PIF.get_record(IF)
+            temp_pif['ip_config_mode'] = IF_record['ip_configuration_mode']
+            temp_pif['is_physical'] = IF_record['physical']
+            temp_pif['device'] = IF_record['device']
+            temp_pif['is_management'] = IF_record['management']
+            temp_pif['IP'] = IF_record['IP']
+            temp_pif['netmask'] = IF_record['netmask']
+            temp_pif['MAC'] = IF_record['MAC']
+            temp_pif['VLAN'] = IF_record['VLAN']
+            temp_pif['id'] = k
+            k += 1
+            temp_record['PIFs'].append(temp_pif)
+        
     return temp_record
 
 
@@ -69,7 +70,7 @@ def get_xenserver_host(xenhost):
         for host_record in hosts:
                 record = session.xenapi.host.get_record(host_record)
                 if record['address'] == xenhost:
-                    temp_host = get_host_info(session, host_record)
+                    temp_host = get_host_info(session, host_record, verbose=True)
                     return temp_host
 
 
@@ -81,7 +82,7 @@ def get_xenserver_host_all():
         temp[ip] = []
         i = 1
         for host in hosts:
-            temp_host = get_host_info(session, host)
+            temp_host = get_host_info(session, host, verbose=False)
             temp_host['id' ]  = i
             i += 1
             temp[ip].append(temp_host)
