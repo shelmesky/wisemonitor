fc1dbc7dbfeed6e7045d4d81d1675579e860532b
roy <roy.lieu@gmail.com>
2013-09-24 19:25:47 +0800

    修改URL正则表达式

--- virtualization/urls.py
+++ virtualization/urls.py
@@ -8,11 +8,11 @@ url_handlers = [
 #(r"/virtual/xenserver/(.*)/vms", XenServer_VMs_Handler),
 # charts of one vm
 # the arguments are: host/uuid/chart type
-(r"/virtual/xenserver/hosts/", XenServer_Get_ALL),
-(r"/virtual/xenserver/hosts/(.*)/", XenServer_Get_Host),
-(r"/virtual/xenserver/(.*)/vms/", XenServer_Get_ALL_vms),
-(r"/virtual/xenserver/(.*)/vms/(.*)/console/", XenServer_Get_VM_Console),
-(r"/virtual/xenserver/(.*)/chart/(.*)/", XenServer_VMs_Chart_Handler),
+(r"^/virtual/xenserver/hosts/$", XenServer_Get_ALL),
+(r"^/virtual/xenserver/hosts/([^/]+)/$", XenServer_Get_Host),
+(r"^/virtual/xenserver/([^/]+)/vms/$", XenServer_Get_ALL_vms),
+(r"^/virtual/xenserver/([^/]+)/vms/([^/]+)/console/$", XenServer_Get_VM_Console),
+(r"^/virtual/xenserver/([^/]+)/chart/([^/]+)/$", XenServer_VMs_Chart_Handler),
 
 #(r"/virtual/libvirt", Libvirt_Handler),
 ]
