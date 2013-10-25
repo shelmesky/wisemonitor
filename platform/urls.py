bb44bcd0497caccb4f52f30a04dc059916a486ee
roy <roy.lieu@gmail.com>
2013-10-25 10:08:29 +0800

    增加CloudStack展示的所有URL和空的views; 更新noVNC的js库: rfb.js

--- platform/urls.py
+++ platform/urls.py
@@ -1,5 +1,28 @@
+#!--encoding:utf-8--
+
 from views import *
 url_handlers = [
-#(r"/platform/cloudstack", CloudStack_Handler),
+# 列出所有的zone
+(r"^/platform/cloudstack/$", CloudStack_Zone_Handler),
+
+# 列出zone的详细信息，包括pad,cluster,host,系统vm,使用量列表
+(r"^/platform/cloudstack/([^/])/detail/$", CloudStack_Zone_Detail_Handler),
+
+# 列出所有的pod
+# 参数: zone
+(r"^/platform/cloudstack/([^/])/$", CloudStack_Pod_Handler),
+
+# 列出所有的cluster
+# 参数：zone/pod
+(r"^/platform/cloudstack/([^/])/([^/])/$", CloudStack_Cluster_Handler),
+
+# 列出所有的host
+# 参数: zone/pod/cluster
+(r"^/platform/cloudstack/([^/])/([^/])/([^/])/$", CloudStack_Host_Handler),
+
+# 列出所有的用户级vm
+# 参数：zone/pod/cluster/host
+(r"^/platform/cloudstack/([^/])/([^/])/([^/])/([^/])/$", CloudStack_VM_Handler),
+
 #(r"/platform/openstack", OpenStack_Handler),
 ]
--- platform/views.py
+++ platform/views.py
@@ -1,7 +1,80 @@
+#!--encoding:utf-8--
+
 import os
 import sys
 import json
 
 from tornado import web
 
-from common.init import *
+from common.init import WiseHandler
+
+
+class CloudStack_Zone_Handler(WiseHandler):
+    """
+    列出所有的zone
+    """
+    def get(self):
+        pass
+
+
+class CloudStack_Zone_Detail_Handler(WiseHandler):
+    """
+    列出zone的详细信息
+    包括pod,cluster,host,系统vm的列表,使用量信息
+    
+    参数：
+    @zone_id zone的ID
+    """
+    def get(self, zone_id):
+        pass
+
+
+class CloudStack_Pod_Handler(WiseHandler):
+    """
+    列出所有的pod
+    
+    参数：
+    @zonei_d Zone的ID
+    """
+    def get(self, zone_id):
+        pass
+
+
+class CloudStack_Cluster_Handler(WiseHandler):
+    """
+    列出所有的cluster
+    
+    参数：
+    @zone_id Zone的ID
+    @pod_id Pod的ID
+    """
+    def get(self, zone_id, pod_id):
+        pass
+
+
+class CloudStack_Host_Handler(WiseHandler):
+    """
+    列出所有的host
+    
+    参数：
+    @zone_id Zone的ID
+    @pod_id Pod的ID
+    @cluster_id Cluster的ID
+    """
+    def get(self, zone_id, pod_id, cluster_id):
+        pass
+
+
+class CloudStack_VM_Handler(WiseHandler):
+    """
+    列出所有的vm
+    
+    参数：
+    @zone_id Zone的ID
+    @pod_id Pod的ID
+    @cluster_id Cluster的ID
+    @host_id Host的ID
+    """
+    def get(self, zone_id, pod_id, cluster_id, host_id):
+        pass
+
