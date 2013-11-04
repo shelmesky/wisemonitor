71e12fdca0a1584fc9fe39a9df8fb0c519f5206c
roy <roy.lieu@gmail.com>
2013-11-04 17:11:06 +0800

    修复Nagios警告Handler的BUG

--- common/alert_handlers/nagios.py
+++ common/alert_handlers/nagios.py
@@ -2,24 +2,26 @@ import json
 from settings import MOTOR_DB as DB
 
 
-def nagios_alert_handler(data):
+def nagios_alert_handler(ch, method, header, data):
     try:
         data = json.loads(data)
     except Exception, e:
         return False
     
-    message = data['data']
-    return_code = data['return_code']
-    if return_code != 0:
-        msg = {
-            'type': 'physical_device',
-            'message_type': data['message_type'],
-            'message': {
-                'host': message['host'],
-                'service': message['service'] if message['service'] else "",
-                'return_code': message['return_code'],
-                'output': message['output']
+    message_type = data['message_type']
+    if message_type == "service_check" or message_type == "host_check":
+        message = data['data']
+        return_code = message['return_code']
+        if return_code != 0:
+            msg = {
+                'type': 'physical_device',
+                'message_type': message_type,
+                'message': {
+                    'host': message['host'],
+                    'service': message['service'] if message['service'] else "",
+                    'return_code': message['return_code'],
+                    'output': message['output']
+                }
             }
-        }
-        DB.nagios_alerts.insert(msg)
+            DB.nagios_alerts.insert(msg)
 
