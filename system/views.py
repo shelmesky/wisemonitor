21c78e441cefc88bfbebeb2cd4d7a6c09d98923d
roy <roy.lieu@gmail.com>
2013-11-05 16:12:14 +0800

    完善性能数据展示页面

--- system/views.py
+++ system/views.py
@@ -3,17 +3,44 @@
 import os
 import sys
 
+import motor
+from tornado import web
+from tornado import gen
+
+from settings import MOTOR_DB as DB
 from common.init import WiseHandler
 
 
 class Physical_Device_Alerts(WiseHandler):
+    @web.asynchronous
+    @gen.coroutine
     def get(self):
-        pass
+        alerts = []
+        cursor = DB.alerts.find({"type": "physical_device"})
+        
+        while(yield cursor.fetch_next):
+            alert = cursor.next_object()
+            alerts.append(alert)
+        
+        alerts.reverse()
+        
+        self.render("system/system_alerts_physical_device.html", alerts=alerts)
 
 
 class XenServer_Alerts(WiseHandler):
+    @web.asynchronous
+    @gen.coroutine
     def get(self):
-        pass
+        alerts = []
+        cursor = DB.alerts.find({"type": "xenserver"})
+        
+        while(yield cursor.fetch_next):
+            alert = cursor.next_object()
+            alerts.append(alert)
+            
+        alerts.reverse()
+        
+        self.render("system/system_alerts_xenserver.html", alerts=alerts)
 
 
 class CloudStack_Alerts(WiseHandler):
