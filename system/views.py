cd680dc56b03b6425e6f419a8fd5139a1476d44b
roy.lieu <roy.lieu@gmail.com>
2014-01-09 12:31:55 +0800

    增加xenserver报警信息翻页

--- system/views.py
+++ system/views.py
@@ -188,13 +188,110 @@ class XenServer_Alerts(WiseHandler):
     @gen.coroutine
     @require_login
     def get(self):
+        keyword = self.get_argument("keyword", "").strip()
+        limit = self.get_argument("limit", "")
+        page = self.get_argument("page", "")
+        
+        if page:
+            try:
+                page = int(page)
+            except:
+                page = 0
+        else:
+            page = 0
+        
+        if limit:
+            try:
+                limit = int(limit)
+            except:
+                limit = 10
+        else:
+            limit = 10
         alerts = []
-        cursor = DB.alerts.find({"type": "xenserver"})
+        
+        cond = {
+            "type": "xenserver",
+        }
+        
+        origin_keyword = None
+        if keyword:
+            origin_keyword = keyword
+            if not keyword.startswith("@"):
+                cond = {
+                    "type": "xenserver",
+                    "$or": [
+                        {"message.host": re.compile(".*%s.*" % keyword)},
+                        {"message.vm_name_label": re.compile(".*%s.*" % keyword)},
+                    ]
+                }
+            else:
+                keyword = keyword[1:]
+                if keyword == "disk":
+                    cond = {
+                        "type": "xenserver",
+                        "message_type": "disk_usage"
+                    }
+                elif keyword == "cpu":
+                    cond = {
+                        "type": "xenserver",
+                        "message_type": "cpu_usage"
+                    }
+                elif keyword == "network":
+                    cond = {
+                        "type": "xenserver",
+                        "message_type": "network_usage"
+                    }
+        else:
+                cond = {
+                    "type": "xenserver",
+                }
+            
+        # 分页开始
+        cursor = DB.alerts.find(cond)
+        record_count = yield motor.Op(cursor.count)
+        cursor = cursor.skip(page * limit).limit(limit)
+        
+        # 一次最多显示几页
+        max_per_page = 10
+        
+        max_pages = record_count / limit
+        if record_count % limit != 0:
+            max_pages += 1
+        
+        # 存储当前显示的页数
+        page_elements = []
+        start = 0
+        end = 0
+        
+        # 如果总页数大于10，则默认显示到第10页结束
+        # 否则直接显示总页数
+        if max_pages >= max_per_page:
+            end = max_per_page
+        else:
+            end = max_pages
+        
+        if page >= max_per_page:
+            current_ten_page = page / max_per_page
+            start = current_ten_page * max_per_page
+            end = start + 10
+            
+            if end > max_pages:
+                remain_page_nums = max_pages % max_per_page
+                if remain_page_nums > 0:
+                    end = start + remain_page_nums
+        
+        for i in range(start, end):
+            page_elements.append(i)
+        
+        current_page = page
+        prev_page = current_page - 1
+        next_page = current_page + 1
+        # 分页结束
         
         while(yield cursor.fetch_next):
             alert = cursor.next_object()
             alerts.append(alert)
-            
+        
         alerts.reverse()
         
         # add page id for every client
@@ -202,7 +299,14 @@ class XenServer_Alerts(WiseHandler):
         if not self.get_secure_cookie("page_id", None) :
             self.set_secure_cookie("page_id", page_id)
         
-        self.render("system/system_alerts_xenserver.html", alerts=alerts)
+        self.render("system/system_alerts_xenserver.html",
+                    alerts=alerts, limit=limit, keyword=origin_keyword,
+                    real_pages=page_elements,
+                    max_pages=max_pages-1,
+                    min_pages=0,
+                    current_page=current_page,
+                    prev_page=prev_page,
+                    next_page=next_page)
     
     @require_login
     @web.asynchronous
