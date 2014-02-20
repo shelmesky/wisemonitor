512e44be9a2ef125fcd3da69bb6c75e44d821729
roy.lieu <roy.lieu@gmail.com>
2014-02-21 01:15:40 +0800

    1. 增加信号处理; 2. log.Fatal会调用os.Exit，使用log.Print代替;

--- bin/VNCPlayback/src/server/server.go
+++ bin/VNCPlayback/src/server/server.go
@@ -8,13 +8,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"syscall"
 	"time"
 	"unsafe"
 )
 
 var (
-	start_time int64
+	start_time  int64
+	signal_chan chan os.Signal
 )
 
 type Head struct {
@@ -34,6 +36,7 @@ func Recorder(ws *websocket.Conn) {
 }
 
 func Processor(ws *websocket.Conn) {
+	defer ws.Close()
 	var head Head
 	const headSize = unsafe.Sizeof(head)
 
@@ -42,7 +45,7 @@ func Processor(ws *websocket.Conn) {
 
 	file, err := os.Open("record.dat.2")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 		return
 	}
 	defer file.Close()
@@ -53,40 +56,40 @@ func Processor(ws *websocket.Conn) {
 
 		n, err := file.Read(buf)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 			return
 		}
 		if n != int(size) {
-			log.Fatal("invlid head size.")
+			log.Print("invlid head size.")
 			return
 		}
 		if err == io.EOF {
-			log.Fatal("got EOF in Read head")
+			log.Print("got EOF in Read head")
 			return
 		}
 
 		err = binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, &head)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 			return
 		}
 
 		buf = make([]byte, head.BodyLength)
 		n, err = file.Read(buf)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 			return
 		}
 		if n != int(head.BodyLength) {
-			log.Fatal("invlid body size.")
+			log.Print("invlid body size.")
 			return
 		}
 		if err == io.EOF {
-			log.Fatal("got EOF in Read body")
+			log.Print("got EOF in Read body")
 			return
 		}
 
-        // ignore packet which send from ws client
+		// ignore packet which send from ws client
 		if head.Type == 1 {
 			continue
 		}
@@ -104,24 +107,53 @@ func Processor(ws *websocket.Conn) {
 
 		time.Sleep(sleep)
 
-        n, err = ws.Write(buf)
-        if err != nil {
-            log.Fatal(err)
-            return
-        }
-        if n != int(head.BodyLength) {
-            log.Fatal("Send body failed")
-            return
-        }
+		n, err = ws.Write(buf)
+		if err == syscall.EPIPE {
+			log.Print("Websocket Got Broken PIPE")
+			return
+		} else if err != nil {
+			log.Print("Websocket Write Failed")
+			log.Print(err)
+			return
+		}
+		if n != int(head.BodyLength) {
+			log.Print("Send body failed")
+			return
+		}
+	}
+}
+
+func signalCallback() {
+	for s := range signal_chan {
+		sig := s.String()
+		if sig == "broken pipe" {
+			continue
+		}
+		log.Print("Got Signal: ", sig)
+		log.Print("Server exit...")
+		os.Exit(0)
 	}
 }
 
 func main() {
-    listen_addr := ":23456"
+	signal_chan = make(chan os.Signal, 10)
+	signal.Notify(signal_chan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		syscall.SIGPIPE,
+		syscall.SIGALRM,
+		syscall.SIGKILL,
+		syscall.SIGPIPE)
+
+	go signalCallback()
+
+	listen_addr := ":23456"
 	http.Handle("/serv/record", websocket.Handler(Recorder))
 	http.Handle("/serv/playback", websocket.Handler(Processor))
 
-    log.Print("Listen on TCP ", listen_addr)
+	log.Print("Listen on TCP ", listen_addr)
 	err := http.ListenAndServe(listen_addr, nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
